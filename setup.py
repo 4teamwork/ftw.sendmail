89052e9497a728b6396cd9eddb2b5e7ab45145ee
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2012-05-08 14:49:31 +0000

    cleanup package structure

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from setuptools import setup, find_packages
 import os
 
-version = open('ftw/sendmail/version.txt').read().strip()
-maintainer = 'Victor Baumann'
+version = '0.5.dev0'
+maintainer = 'Mathias Leimgruber'
 
 setup(name='ftw.sendmail',
       version=version,
       description="Maintainer: %s" % maintainer,
-      long_description=open("README.txt").read() + "\n" + \
+      long_description=open("README.rst").read() + "\n" + \
           open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      # Get more strings from
+      # http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
@@ -18,7 +19,7 @@ setup(name='ftw.sendmail',
       keywords='',
       author='%s, 4teamworkk GmbH' % maintainer,
       author_email='mailto:info@4teamwork.ch',
-      url='http://psc.4teamwork.ch/4teamwork/ftw/ftw.notification.email/',
+      url='https://github.com/4teamwork/ftw.sendmail',
       license='GPL2',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['ftw'],
@@ -27,6 +28,7 @@ setup(name='ftw.sendmail',
       install_requires=[
         'setuptools',
         'stoneagehtml',
+        'BeautifulSoup'
         # -*- Extra requirements: -*-
         ],
       entry_points="""
