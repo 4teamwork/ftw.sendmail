e43edadd331ad13a9aa3db0a6a43ffe393ffc8d3
Jonas Baumann <jone@jone.ch>
2012-11-05 08:46:19 +0100

    Update setup.py.

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ from setuptools import setup, find_packages
 import os
 
 version = '0.5.dev0'
-maintainer = 'Mathias Leimgruber'
 
 
 tests_require = [
@@ -10,11 +9,13 @@ tests_require = [
     'Products.PloneTestCase',
     ]
 
+
 setup(name='ftw.sendmail',
       version=version,
-      description="Maintainer: %s" % maintainer,
+      description='A utility for sending multipart emails in Plone.',
       long_description=open("README.rst").read() + "\n" + \
           open(os.path.join("docs", "HISTORY.txt")).read(),
+
       # Get more strings from
       # http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
@@ -22,27 +23,31 @@ setup(name='ftw.sendmail',
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
-      keywords='',
-      author='%s, 4teamworkk GmbH' % maintainer,
+
+      keywords='ftw sendmail',
+      author='4teamwork GmbH',
       author_email='mailto:info@4teamwork.ch',
       url='https://github.com/4teamwork/ftw.sendmail',
       license='GPL2',
+
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['ftw'],
       include_package_data=True,
       zip_safe=False,
+
       install_requires=[
         'setuptools',
         'stoneagehtml',
         'BeautifulSoup'
         # -*- Extra requirements: -*-
         ],
+
       tests_require=tests_require,
       extras_require=dict(tests=tests_require),
 
       entry_points="""
       # -*- Entry points: -*-
       [z3c.autoinclude.plugin]
-      target = ftw
+      target = plone
       """,
       )
