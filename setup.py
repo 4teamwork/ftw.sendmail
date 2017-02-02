deb337fcf2113ba410eb6a9a057a369992bd28a4
Jonas Baumann <jone@jone.ch>
2017-02-02 09:59:08 +0100

    Drop Plone 4.1 support.

--- setup.py
+++ setup.py
@@ -22,8 +22,8 @@ setup(name='ftw.sendmail',
 
       classifiers=[
         'Framework :: Plone',
-        'Framework :: Plone :: 4.1',
         'Framework :: Plone :: 4.2',
+        'Framework :: Plone :: 4.3',
         'Programming Language :: Python',
         'Topic :: Software Development :: Libraries :: Python Modules',
         ],
